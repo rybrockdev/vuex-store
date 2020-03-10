cb60e6d67975b905b4d4194395d59bcae17bffb0
Ry Brockers <rybrockers@github.com>
2020-03-10 21:37:20 +0000

    added icons. Started to style navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app>
+      <ul>
+        <router-link tag="li" to="/">Home</router-link>
+        <router-link tag="li" to="/about">About</router-link>
+      </ul>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title class="headline">
+        <span>Vuex-Store</span>
+        <span class="font-weight-light">Ryan Brockley</span>
+      </v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+// import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
+
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+
+                primary: '#333333',
+                secondary: '#2b2b2b',
+                white: '#ffffff',
+                orange: '#Fc6d2b',
+                darkgrey: '#707070',
+                complete: '#2bfc83',
+                inprogress: '#ffb936',
+                incomplete: '#ff1212',
+                lightgrey: '#cacaca',
+
+
+            }
+        }
+    }
+});
